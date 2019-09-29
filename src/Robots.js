a2ce45198aeb1dfa3b96ca81434c5f570d10b8e7
pwrmvspark <danielparkonline@gmail.com>
2019-09-28 23:39:35 -0700

    initial commit, pushed from terminal

---
+++ src/Robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Maximus Destructo",
+    username: "md3",
+    email: "md3@email.com"
+  },
+  {
+    id: 2,
+    name: "Natalie Portman",
+    username: "natport",
+    email: "natport@email.com"
+  },
+  {
+    id: 3,
+    name: "Charlie Park",
+    username: "cp1",
+    email: "cp1@email.com"
+  },
+  {
+    id: 4,
+    name: "Kevin Gates",
+    username: "kevgates",
+    email: "kevgates@email.com"
+  },
+  {
+    id: 5,
+    name: "Nate Diaz",
+    username: "diaztri",
+    email: "diaztri@email.com"
+  },
+  {
+    id: 6,
+    name: "Khabib Normy",
+    username: "khabib",
+    email: "normys@email.com"
+  },
+  {
+    id: 7,
+    name: "Julius Caesar",
+    username: "traitor",
+    email: "traitor@email.com"
+  },
+  {
+    id: 8,
+    name: "Merpa Derpa",
+    username: "mdmdmd",
+    email: "mdmdmd@email.com"
+  },
+  {
+    id: 9,
+    name: "Jet Li",
+    username: "tigerbalm",
+    email: "tigerbalm@email.com"
+  },
+  {
+    id: 10,
+    name: "Toyota Supra",
+    username: "fastcar",
+    email: "fastcar@email.com"
+  },
+  {
+    id: 11,
+    name: "Nick Cannon",
+    username: "millions",
+    email: "millions@email.com"
+  },
+  {
+    id: 12,
+    name: "Happy Gilmore",
+    username: "adamsand",
+    email: "adamsand@email.com"
+  }
+]
